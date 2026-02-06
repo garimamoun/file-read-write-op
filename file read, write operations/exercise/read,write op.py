0e16f94ce11ccca4a96e63ea1365c3a9f17bc59b
garimamoun <garimamoun12@gmail.com>
2026-02-06 20:05:58 +0530

    Add files via upload

---
+++ file read, write operations/exercise/read,write op.py
@@ -0,0 +1,19 @@
+# Write data into a file
+file = open("example.txt", "w")
+
+text = input(" Enter text to store in the file: ")
+file.write(text)
+
+file.close()
+
+print("✅ Data written to file successfully!\n")
+
+# Read data from the file
+file = open("example.txt", "r")
+
+content = file.read()
+
+file.close()
+
+print("📄 Reading data from file:")
+print(content)
